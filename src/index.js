eedcac839fade4ff8fb8ef6d0bb014d0f826789b
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2017-02-15 06:36:05 +0700

    New Changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//const color = Math.random() > 0.5 ? 'green' : 'red';
-
-const App = () => {
-    return ( 
-        <h2 className = "text-center" >
-        Hello React with JSX!!
-        </h2>
-    );
-};
+import App from './components/App';
 
 ReactDOM.render(
     // React.createElement('h2', null, 'Hello ReactasdSD'),
